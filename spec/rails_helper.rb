bb5ef8daa4732620dc0e80d6367edc3759431026
João Pedro Matias <jpmmatias2018@outlook.com>
2021-06-06 19:04:31 -0300

    Visitante visita site

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,12 +32,15 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.include Rails.application.routes.url_helpers
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include Warden::Test::Helpers
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
+  config.before(type: :system) do
+    driven_by :rack_test
+  end
   config.use_transactional_fixtures = true
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
---
+++ spec/system/visitor_visit_website_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe "Visitor visits 'PayNow!' homepage" do
+    it "successfuly" do
+        visit root_path
+
+        expect(page).to have_text("PayNow!")
+    end
+
+
+end
