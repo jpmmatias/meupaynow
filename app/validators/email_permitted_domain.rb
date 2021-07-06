a5ab8b09c6b0681481600206686b0d9adb2df158
João Pedro Matias <jpmmatias2018@outlook.com>
2021-07-06 11:30:40 -0300

    Ajustes após simplecov

--- app/validators/email_permitted_domain.rb
+++
@@ -1,8 +0,0 @@
-class EmailDomain < ActiveModel::Validator
-    def validate(record)
-        email_domains = File.open(Rails.root.join("app", "assets", "texts","email_providers.txt")).readlines.map(&:chomp)
-      unless email_domains.include? record.email
-        record.errors.add :email, "Apenas email empresarial permitido"
-      end
-    end
-  end
--- spec/models/payment_method_spec.rb
+++ spec/models/payment_method_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe PaymentMethod, type: :model do
   context 'validation' do
-    it { should validate_presence_of(:icon)}
+    it { should validate_presence_of(:icon) }
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:payment_type) }
     it { should validate_presence_of(:tax) }
