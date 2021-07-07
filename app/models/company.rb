a820885b639ca4d437ffde6f5c728cbd0dc237f2
João Pedro Matias <jpmmatias2018@outlook.com>
2021-07-07 09:38:16 -0300

    Refatoração company e secure_token

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,11 +1,11 @@
 class Company < ApplicationRecord
   extend FriendlyId
   friendly_id :token, use: %i[slugged history finders]
-  has_many :subscriptions
+  has_many :subscriptions, dependent: :destroy
   has_many :customers, through: :subscriptions
-  has_many :users
-  has_many :products
-  has_many :company_payment_methods
+  has_many :users, dependent: false
+  has_many :products, dependent: :destroy
+  has_many :company_payment_methods, dependent: :destroy
   has_secure_token
   validates :cnpj, :corporate_name, :email, :address, presence: true,
                                                       uniqueness: true
